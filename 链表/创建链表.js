f4af04d90c435581cd6dca328fb0f8fa95db8a63
jinxin <jinxin@wisers.com>
2017-04-27 18:20:56 +0800

    链表

---
+++ 链表/创建链表.js
@@ -0,0 +1,33 @@
+/**
+ * Created by Jinxin on 2017/4/27.
+ */
+
+function linkList() {
+    let Node = function (element) {
+        this.node = element;
+        this.next = null;
+    };
+    let length = 0;
+    let head = null;
+    this.append = function (element) {
+        let node = new Node(element);
+        let nodelist = {};
+        if (head === null) {
+            head = node;
+        } else {
+            nodelist = head;
+            while (nodelist.next) {
+                nodelist = nodelist.next;
+            }
+            nodelist.next = node;
+        }
+        length++;
+        return head;
+    };
+}
+let list = new linkList();
+console.log(list.append("This is first node!"));
+console.log(list.append("This is second node!"));
+console.log(list.append("This is third node!"));
+console.log(list.append("This is fouth node!"));
+console.log(list.append("This is fifth node!"));
